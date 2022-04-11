8d31a1520037f32f92bf32ec1930b88b39f59c8a
Anjali <patilanjali1632@gmail.com>
2022-04-11 20:27:46 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Display from './components/displaybtn/display';
 import Header from './components/Header/header';
 import Services from './components/Services';
 import Home from './components/Home/home';
+import SignIn from './components/SignIn/signin';
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
         <Route path='/' exact element={<Home/>} />
         <Route path='/buttons' element={<Display/>} /> 
         <Route path='/services' element={<Services/>} />
-        {/* <Route path='/contact-us' element={Contact} />
-        <Route path='/sign-up' element={SignUp} /> */}
+        {/* <Route path='/contact-us' element={Contact}/>*/ }
+        <Route path='/sign-in' element={<SignIn/>} />
       </Routes>
     </Router>
     
